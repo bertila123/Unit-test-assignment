c3a011a7fe7f438accd5ac337cdf0ee68a0cedb8
Bertila Ngong <ngongberti@gmail.com>
2024-11-06 13:45:05 -0200

    flake

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(message="Hello level 400 FET, Quality Assurance!")
+    return jsonify(message="Helloo level 400 FET, Quality Assurance!")
 
 
 if __name__ == '__main__':
